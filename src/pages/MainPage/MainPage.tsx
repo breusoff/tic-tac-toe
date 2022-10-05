6674af3c9c2176d26a820a53429573da990b2e6c
Vladimir Breusov <breusov.vova@gmail.com>
2022-10-06 01:19:50 +0300

    feat: base pages

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Page from "src/components/Page";
+
+const MainPage = () => {
+    return <Page>MainPage</Page>;
+};
+
+export default MainPage;
---
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Page from "src/components/Page";
+
+const SettingsPage = () => {
+    return <Page>SettingsPage</Page>;
+};
+
+export default SettingsPage;

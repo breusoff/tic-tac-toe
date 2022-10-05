20f76f896bfb9aeae0597a9d2b7ff4285bbc3c37
Vladimir Breusov <breusov.vova@gmail.com>
2022-10-06 01:15:17 +0300

    feat: roboto font

--- src/styles/Global.styles.ts
+++ src/styles/Global.styles.ts
@@ -1,8 +1,22 @@
 import {createGlobalStyle, css} from "styled-components";
+import RobotoEot from "src/styles/fonts/Roboto/Roboto-Bold.eot";
+import RobotoTtf from "src/styles/fonts/Roboto/Roboto-Bold.ttf";
+import RobotoWoff from "src/styles/fonts/Roboto/Roboto-Bold.woff";
 
 const global = css`
+    @font-face {
+        font-family: "Roboto";
+        src: url(${RobotoEot});
+        src: local("Roboto Bold"), local("Roboto-Bold"),
+            url("${RobotoEot}?#iefix") format("embedded-opentype"),
+            url(${RobotoWoff}) format("woff"),
+            url(${RobotoTtf}) format("truetype");
+        font-weight: bold;
+        font-style: normal;
+    }
+
     html {
-        font-family: "Lato", sans-serif;
+        font-family: "Roboto", sans-serif;
         font-size: 10px;
     }
 

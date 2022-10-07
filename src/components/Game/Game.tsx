a65ff6c341fc624b47b2bf1ca8b0b235203c43d5
Vladimir Breusov <breusov.vova@gmail.com>
2022-10-08 01:22:16 +0300

    feat: cell click

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,23 +1,50 @@
 /* eslint-disable react/no-array-index-key */
-import React from "react";
+import React, {useState} from "react";
 import GameCell from "src/components/Game/GameCell";
 import {IGameCellState} from "src/components/Game/GameCell/IGameCellState";
 import {GameRaw, GameWrapper} from "./Game.styles";
 
-const state: IGameCellState[][] = [
-    ["x", null, "o"],
-    [null, "x", null],
-    ["o", null, "x"],
+const defaultState: IGameCellState[][] = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
 ];
 
+type GameStep = "x" | "o";
+
 const Game = () => {
+    const [state, setState] = useState<IGameCellState[][]>(defaultState);
+    const [step, setStep] = useState<GameStep>("x");
+
+    function toggleStep() {
+        setStep((prevState) => (prevState === "x" ? "o" : "x"));
+    }
+
+    function onCellClick(rawIndex: number, cellIndex: number) {
+        setState((prevState) => {
+            prevState[rawIndex][cellIndex] = step;
+            return [...prevState];
+        });
+        toggleStep();
+    }
+
     return (
         <GameWrapper>
             {state.map((raw, rawIndex) => (
                 <GameRaw key={rawIndex}>
-                    {raw.map((cell, cellIndex) => (
-                        <GameCell key={cellIndex} cell={cell} />
-                    ))}
+                    {raw.map((cell, cellIndex) => {
+                        function onClick() {
+                            onCellClick(rawIndex, cellIndex);
+                        }
+
+                        return (
+                            <GameCell
+                                key={cellIndex}
+                                cell={cell}
+                                onClick={onClick}
+                            />
+                        );
+                    })}
                 </GameRaw>
             ))}
         </GameWrapper>
--- src/components/Game/GameCell/GameCell.tsx
+++ src/components/Game/GameCell/GameCell.tsx
@@ -2,16 +2,18 @@ import React, {FC} from "react";
 import OIcon from "src/assets/icons/OIcon";
 import XIcon from "src/assets/icons/XIcon";
 import {IGameCellState} from "src/components/Game/GameCell/IGameCellState";
+import {EmptyCallback} from "src/types/EmptyCallback";
 import {GameCellO, GameCellWrapper, GameCellX} from "./GameCell.styles";
 
 interface IGameCellProps {
     cell?: IGameCellState;
     win?: boolean;
+    onClick?: EmptyCallback;
 }
 
-const GameCell: FC<IGameCellProps> = ({cell, win}) => {
+const GameCell: FC<IGameCellProps> = ({cell, win, onClick}) => {
     if (!cell) {
-        return <GameCellWrapper />;
+        return <GameCellWrapper onClick={onClick} />;
     }
 
     if (cell === "x") {

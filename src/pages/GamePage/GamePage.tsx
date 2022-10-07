fd3cb730266ccd90161a00e1827c295d80d9cb98
Vladimir Breusov <breusov.vova@gmail.com>
2022-10-07 22:48:50 +0300

    feat: game page

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import {useTranslation} from "react-i18next";
 import Header from "src/components/Header";
 import Page from "src/components/Page";
+import Step from "src/components/Step";
 import Wins from "src/components/Wins";
+import {GamePageArea, GamePageContent} from "./GamePage.styles";
 
 const GamePage = () => {
     const {t} = useTranslation();
@@ -10,8 +12,11 @@ const GamePage = () => {
     return (
         <Page>
             <Header title={t("main.title")} settingsButton backButton />
-            GamePage
-            <Wins />
+            <GamePageContent>
+                <GamePageArea>GamePage</GamePageArea>
+                <Step />
+                <Wins />
+            </GamePageContent>
         </Page>
     );
 };

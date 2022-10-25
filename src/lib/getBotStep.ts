f9dfcfd5efcfdf427c801ea5cc498ce44ceba8d5
Vladimir Breusov <breusov.vova@gmail.com>
2022-10-26 01:23:29 +0300

    feat: temp

--- src/lib/getBotStep.ts
+++ src/lib/getBotStep.ts
@@ -1,5 +1,7 @@
+import isEqual from "src/lib/isEqual";
 import {getRandomArrayElement} from "src/lib/random";
-import {GameState} from "src/types/GameState";
+import {GameCellState} from "src/types/GameCellState";
+import {GameState, defaultState} from "src/types/GameState";
 import {GameStep} from "src/types/GameStep";
 
 interface IBotStep {
@@ -9,30 +11,74 @@ interface IBotStep {
 
 type BotStep = IBotStep | undefined;
 
+function gerRandomStep(state: GameState): BotStep {
+    const steps: IBotStep[] = [];
+
+    state.forEach((row, rowIndex) =>
+        row.forEach((cell, cellIndex) => {
+            if (cell !== null) return;
+            steps.push({
+                row: rowIndex,
+                cell: cellIndex,
+            });
+        }),
+    );
+
+    if (!steps.length) return undefined;
+
+    return getRandomArrayElement(steps);
+}
+
+function shouldMakeStepOnLine(line: GameCellState[]): boolean {
+    // [null, null, null]
+    const emptyLine = line.filter((i) => i === null).length === line.length;
+    if (emptyLine) return false;
+
+    // [0,1][1,2][0,2]
+    const [first, second, third] = line;
+    return first === second || second === third || first === third;
+}
+
 const bot = {
     /**
      * random moves, silly bot
      */
     easy: (state: GameState): BotStep => {
-        const steps: IBotStep[] = [];
+        return gerRandomStep(state);
+    },
+    /**
+     * better moves
+     */
+    medium: (state: GameState, step: GameStep): BotStep => {
+        // random on first turn
+        if (isEqual(state, defaultState)) {
+            return gerRandomStep(state);
+        }
 
-        state.forEach((row, rowIndex) =>
-            row.forEach((cell, cellIndex) => {
-                if (cell !== null) return;
-                steps.push({
-                    row: rowIndex,
-                    cell: cellIndex,
-                });
-            }),
-        );
+        const size = state.length;
 
-        if (!steps.length) return undefined;
+        console.log("step", step);
 
-        return getRandomArrayElement(steps);
+        //[x, null, o]
+        //[null, null, null]
+        //[o, o, null]
+        //[x, o, x]
+
+        shouldMakeStepOnLine([GameStep.x, null, null]);
+
+        // // horizontal
+        // for (let i = 0; i < size; i += 1) {
+        //     const line = state[i];
+        //
+        //     console.log("line", line, shouldMakeStepOnLine(line));
+        // }
+
+        return gerRandomStep(state);
     },
 };
 
 const getBotStep = (state: GameState, step: GameStep): BotStep => {
+    return bot.medium(state, step);
     return bot.easy(state);
 };
 
